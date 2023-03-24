905790d2380a2b26f40a5b1b245499fe5e42442b
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2023-03-24 11:45:54 -0600

    End of class update.  Started PlaylistManagement page

--- BlazorWebApp/BlazorWebApp/Pages/SamplePages/PlaylistManagement.razor.cs
+++ BlazorWebApp/BlazorWebApp/Pages/SamplePages/PlaylistManagement.razor.cs
@@ -1,6 +1,49 @@
-﻿namespace BlazorWebApp.Pages.SamplePages
+﻿using Microsoft.AspNetCore.Components;
+using PlaylistManagementSystem.BLL;
+using PlaylistManagementSystem.Paginator;
+using PlaylistManagementSystem.ViewModels;
+
+namespace BlazorWebApp.Pages.SamplePages
 {
     public partial class PlaylistManagement
     {
+        //  we are now injecting our service into our class using the [Inject] attribute
+        [Inject]
+        protected PlaylistTrackService? PlaylistTrackService { get; set; }
+
+        #region Fields
+        // search pattern
+        private string searchPattern { get; set; } = "Deep";
+        // search type
+        private string searchType { get; set; } = "Artist";
+        //  play list name
+        private string playlistName { get; set; } = "HansenB1";
+        //  playlist ID (would be return from database but hard coding it)
+        private int playlistId { get; set; } = 13;
+        //  user name
+        private string userName { get; set; } = "HansenB";
+        //  feed back messages
+        private string feedBack { get; set; }
+        #endregion
+
+        protected List<PlaylistTrackView> Playlists { get; set; } = new();
+
+        #region    Paginator
+        //  desired current page size
+        private const int PAGE_SIZE = 10;
+
+        //  sort colum used with the paginator
+        protected string SortField { get; set; } = "Owner";
+
+        // sort direction for paginator
+        protected string Direction { get; set; } = "desc";
+        //  current page for painator
+        protected int CurrentPage { get; set; } = 1;
+
+        //paginator collection of track selection view
+        protected PagedResult<TrackSelectionView> PaginatorTrackSelection { get; set; } = new();
+
+        #endregion
+
     }
 }
